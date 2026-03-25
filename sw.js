923be22d91183d139131b1e094090fdd0df859e0
parkustinaaa <parkustinaaa@gmail.com>
2026-03-25 22:00:57 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// wnstudio sw.js - updated: 1774443213
-const CACHE = 'wnstudio-1774443213';
+// wnstudio sw.js - updated: 1774443538
+const CACHE = 'wnstudio-1774443538';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/novel-studio/', '/novel-studio/index.html'])));
