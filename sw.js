afc79e4377458c9453a60720693c1f43eb0cd1d0
parkustinaaa <parkustinaaa@gmail.com>
2026-03-26 13:43:15 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// wnstudio sw.js - updated: 1774499696
-const CACHE = 'wnstudio-1774499696';
+// wnstudio sw.js - updated: 1774500084
+const CACHE = 'wnstudio-1774500084';
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/novel-studio/', '/novel-studio/index.html']))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => { if(e.request.method!=='GET') return; e.respondWith(fetch(e.request).then(res => { if(res&&res.status===200){const clone=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,clone));} return res; }).catch(()=>caches.match(e.request).then(c=>c||caches.match('/novel-studio/index.html')))); });
