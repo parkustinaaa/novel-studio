cee44d178890b2a98c0c78347257fe03a831a9e5
parkustinaaa <parkustinaaa@gmail.com>
2026-03-26 14:33:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,6 @@
-// wnstudio sw.js - updated: 1774502673
-const CACHE = 'wnstudio-1774502673';
-
-self.addEventListener('install', e => {
-  // 즉시 활성화
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll(['/novel-studio/', '/novel-studio/index.html'])
-    )
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  // 이전 캐시 전부 삭제
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => {
-        console.log('캐시 삭제:', k);
-        return caches.delete(k);
-      }))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  // 항상 네트워크 우선
-  e.respondWith(
-    fetch(e.request, {cache: 'no-cache'})
-      .then(res => {
-        if (res && res.status === 200) {
-          const clone = res.clone();
-          caches.open(CACHE).then(cache => cache.put(e.request, clone));
-        }
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
-
-self.addEventListener('message', e => {
-  if (e.data?.type === 'SKIP_WAITING') self.skipWaiting();
-});
+// wnstudio sw.js - updated: 1774503096
+const CACHE = 'wnstudio-1774503096';
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/novel-studio/', '/novel-studio/index.html']))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { if(e.request.method!=='GET') return; e.respondWith(fetch(e.request, {cache:'no-cache'}).then(res => { if(res&&res.status===200){const clone=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,clone));} return res; }).catch(()=>caches.match(e.request))); });
+self.addEventListener('message', e => { if(e.data?.type==='SKIP_WAITING') self.skipWaiting(); });
