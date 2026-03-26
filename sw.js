9f7c4521cac53cfb27a0f6844cf0c5e00bee8fd8
parkustinaaa <parkustinaaa@gmail.com>
2026-03-26 14:49:00 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// wnstudio sw.js - updated: 1774503096
-const CACHE = 'wnstudio-1774503096';
+// wnstudio sw.js - updated: 1774503975
+const CACHE = 'wnstudio-1774503975';
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/novel-studio/', '/novel-studio/index.html']))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => { if(e.request.method!=='GET') return; e.respondWith(fetch(e.request, {cache:'no-cache'}).then(res => { if(res&&res.status===200){const clone=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,clone));} return res; }).catch(()=>caches.match(e.request))); });
+self.addEventListener('fetch', e => { if(e.request.method!=='GET') return; e.respondWith(fetch(e.request,{cache:'no-cache'}).then(res => { if(res&&res.status===200){const clone=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,clone));} return res; }).catch(()=>caches.match(e.request))); });
 self.addEventListener('message', e => { if(e.data?.type==='SKIP_WAITING') self.skipWaiting(); });
